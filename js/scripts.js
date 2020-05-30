0ffc5b41249a36674d6c7dbfb7beb67e08a27664
Ivar Johnson <ivar@ivardesign.com>
2020-05-30 14:40:16 -0700

    added demo for flex-wrap updates to CSS and JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,16 @@ const dirSpan = document.getElementById('dirSpan');
 const flexDirBtn = document.getElementById('flexDirection');
 const flexDirSpan = document.getElementById('flexDirSpan'); 
 const flexDirRevBtn = document.getElementById('flexDirectionReversed');
+const flexWrapBtn = document.getElementById('flexWrapButton')
 const containerList = document.getElementsByClassName('container');
 const displayFlexList = document.getElementsByClassName('display-flex');
 const flexDirRevButton = document.getElementById('flexDirRevButton');
 let direction = 'row';
-let flexDirRevBttnChecked = false;
+let flexDirRevBtnChecked = false;
+let flexWrapBtnChecked = false;
 
-if (flexDirRevBttnChecked) {
+// directionality
+if (flexDirRevBtnChecked) {
     flexDirRevButton.classList.add('checked');
 } else {
     flexDirRevButton.classList.remove('checked');
@@ -28,9 +31,9 @@ dirBtn.addEventListener('click', function () {
     }
 });
 
+// flex-direction column/row
 flexDirBtn.addEventListener('click', function () {
-
-    if (flexDirRevBttnChecked == false) {
+    if (flexDirRevBtnChecked == false) {
       
         if (direction == 'row') {
 
@@ -78,51 +81,68 @@ flexDirBtn.addEventListener('click', function () {
     }
 });
 
+// flex-direction column-reverse/row-reverse
 flexDirRevBtn.addEventListener('click', function() {
-    if (flexDirRevBttnChecked == false) {
+    if (flexDirRevBtnChecked == false) {
 
-        for (let k = 0; k < displayFlexList.length; k++) {
-            displayFlexList[k].classList.remove('flexDirectionRow');
-            displayFlexList[k].classList.remove('flexDirectionColumn');
+        for (let i = 0; i < displayFlexList.length; i++) {
+            displayFlexList[i].classList.remove('flexDirectionRow');
+            displayFlexList[i].classList.remove('flexDirectionColumn');
         }
 
         if (direction == 'row') {
-            for (let m = 0; m < displayFlexList.length; m++) {
-                displayFlexList[m].classList.toggle('flexDirectionRow-reverse');
+            for (let ii = 0; ii < displayFlexList.length; ii++) {
+                displayFlexList[ii].classList.toggle('flexDirectionRow-reverse');
             }
 
         } else {
 
-            for (let n = 0; n < displayFlexList.length; n++) {
-                displayFlexList[n].classList.toggle('flexDirectionColumn-reverse');
+            for (let j = 0; j < displayFlexList.length; j++) {
+                displayFlexList[j].classList.toggle('flexDirectionColumn-reverse');
             }
 
         }
 
         flexDirRevButton.classList.toggle('checked');
-        flexDirRevBttnChecked = true; 
+        flexDirRevBtnChecked = true; 
 
     } else {
 
-        for (let p = 0; p < displayFlexList.length; p++) {
-            displayFlexList[p].classList.remove('flexDirectionRow-reverse');
-            displayFlexList[p].classList.remove('flexDirectionColumn-reverse');
+        for (let jj = 0; jj < displayFlexList.length; jj++) {
+            displayFlexList[jj].classList.remove('flexDirectionRow-reverse');
+            displayFlexList[jj].classList.remove('flexDirectionColumn-reverse');
         }
 
         if (direction == 'row') {
-            for (let q = 0; q < displayFlexList.length; q++) {
-                displayFlexList[q].classList.toggle('flexDirectionRow');
+            for (let k = 0; k < displayFlexList.length; k++) {
+                displayFlexList[k].classList.toggle('flexDirectionRow');
             }
 
         } else {
 
-            for (let r = 0; r < displayFlexList.length; r++) {
-                displayFlexList[r].classList.toggle('flexDirectionColumn');
+            for (let kk = 0; kk < displayFlexList.length; kk++) {
+                displayFlexList[kk].classList.toggle('flexDirectionColumn');
             }
         }
 
         flexDirRevButton.classList.toggle('checked');
-        flexDirRevBttnChecked = false;
+        flexDirRevBtnChecked = false;
 
     }
+});
+
+// flex-wrap
+flexWrapBtn.addEventListener('click', function () {
+    for (let i = 0; i < displayFlexList.length; i++) {
+        displayFlexList[i].classList.toggle('flex-wrap');
+    }
+
+    flexWrapBtn.classList.toggle('checked');
+
+    if (flexWrapBtnChecked == true) {
+        flexWrapBtnChecked = false;
+    } else {
+        flexWrapBtnChecked = true;
+    }
+
 });
